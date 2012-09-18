95a0b8f2d4cdfc02b9fbcb2427e25231e2870d1c
Lawrence Bower <lawrencebower@gmail.com>
2012-09-18 15:22:20 +0100

    adding web module

--- web/src/main/java/org/lawrencebower/docgen/web/Test/Test.java
+++ web/src/main/java/org/lawrencebower/docgen/web/Test/Test.java
@@ -2,6 +2,6 @@ package org.lawrencebower.docgen.web.Test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test.main");
+         System.out.println("Test.main");
     }
 }
