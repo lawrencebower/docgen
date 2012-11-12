e19bdde70072dc810a4a21a4049f9a1a52f0ae7c
Lawrence Bower <lawrencebower@gmail.com>
2012-11-12 10:59:35 +0000

    code formatting

--- web/src/main/java/org/lawrencebower/docgen/web/controller/DataEntryController.java
+++ web/src/main/java/org/lawrencebower/docgen/web/controller/DataEntryController.java
@@ -4,8 +4,10 @@ import org.apache.log4j.Logger;
 import org.lawrencebower.docgen.core.document.PDFDocument;
 import org.lawrencebower.docgen.web.model.SessionData;
 import org.lawrencebower.docgen.web_logic.business.controler_business.data_entry.DataEntryCB;
+import org.lawrencebower.docgen.web_model.view.contact.ContactView;
 import org.lawrencebower.docgen.web_model.view.document_info.DocComponentView;
 import org.lawrencebower.docgen.web_model.view.document_info.DocumentInfoView;
+import org.lawrencebower.docgen.web_model.view.product.ProductView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
@@ -25,31 +27,40 @@ public class DataEntryController {
 
     static Logger logger = Logger.getLogger(DataEntryController.class);
 
-    @Autowired
     private DataEntryCB business;
-    @Autowired
     private SessionData sessionData;
 
-    public DataEntryController() {
+    @Autowired
+    public void setBusiness(DataEntryCB business) {
+        this.business = business;
+    }
+
+    @Autowired
+    public void setSessionData(SessionData sessionData) {
+        this.sessionData = sessionData;
     }
 
     @RequestMapping(value = "/prepareFields", method = RequestMethod.GET)
     public String prepareFields() {
 
+        ContactView selectedCustomer = sessionData.getSelectedCustomer();
+        List<ProductView> selectedProducts = sessionData.getSelectedProducts();
+        ContactView selectedBusiness = sessionData.getSelectedBusiness();
+
         List<DocumentInfoView> documentInfos =
-                business.getDocumentsForViewing(sessionData.getSelectedCustomer(),
-                                                sessionData.getSelectedProducts());
+                business.getDocumentsForViewing(selectedCustomer,
+                                                selectedProducts);
 
         sessionData.setDocuments(documentInfos);
 
         business.mapAutoMapFields(documentInfos,
-                                  sessionData.getSelectedCustomer(),
-                                  sessionData.getSelectedBusiness());
+                                  selectedCustomer,
+                                  selectedBusiness);
 
         return "dataEntry";
     }
 
-    @RequestMapping(value = "/toggleAutomapped")
+    @RequestMapping("/toggleAutomapped")
     public String toggleShowAutomappedFields(){
         boolean currentValue = sessionData.isShowAutoMappedFields();
         sessionData.setShowAutoMappedFields(!currentValue);
@@ -63,14 +74,17 @@ public class DataEntryController {
 
         Map<String, String[]> parameterMap = webRequest.getParameterMap();
 
-        business.mapFieldValuesToComponents(parameterMap,
-                                            sessionData.getDocuments());
+        List<DocumentInfoView> relevantDocs = sessionData.getDocuments();
 
-        List<PDFDocument> pdFs = business.createPDFs(sessionData.getDocuments());
+        business.mapFieldValuesToComponents(parameterMap, relevantDocs);
+
+        List<PDFDocument> pdFs = business.createPDFs(relevantDocs);
 
         sessionData.setGeneratedDocuments(pdFs);
 
-        List<File> allPdfFiles = business.writePDFsToFiles(sessionData.getPDFDocuments());
+        List<PDFDocument> generatedPDFs = sessionData.getPDFDocuments();
+
+        List<File> allPdfFiles = business.writePDFsToFiles(generatedPDFs);
 
         File concatenatedFile = business.makeConcatenatedFile(allPdfFiles);
 
@@ -91,8 +105,11 @@ public class DataEntryController {
 
     public List<DocComponentView> getDocComponentViews(){
 
-        return business.getComponentsForViewing(sessionData.getDocuments(),
-                                                sessionData.isShowAutoMappedFields());
+        List<DocumentInfoView> relevantDocs = sessionData.getDocuments();
+
+        boolean showAutoMappedFields = sessionData.isShowAutoMappedFields();
+
+        return business.getComponentsForViewing(relevantDocs, showAutoMappedFields);
     }
 
 }
