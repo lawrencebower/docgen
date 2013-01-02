ec0ffd6a2fabd9d913883dd9868b5b025956da4d
Lawrence Bower <lawrencebower@gmail.com>
2013-01-02 11:19:27 +0000

    adding stubs for tsv factory

---
+++ web-logic/src/main/java/org/lawrencebower/docgen/web_logic/view/view_factory/tsv_factory/DocumentFactoryTsvImpl.java
@@ -0,0 +1,18 @@
+package org.lawrencebower.docgen.web_logic.view.view_factory.tsv_factory;
+
+import org.lawrencebower.docgen.web_logic.view.document.DocumentView;
+import org.lawrencebower.docgen.web_logic.view.view_factory.factory.DocumentFactory;
+
+import java.util.List;
+
+public class DocumentFactoryTsvImpl implements DocumentFactory {
+    @Override
+    public List<DocumentView> getAllDocuments() {
+        return null;
+    }
+
+    @Override
+    public DocumentView createDocument(String documentName) {
+        return null;
+    }
+}
---
+++ web-logic/src/main/java/org/lawrencebower/docgen/web_logic/view/view_factory/tsv_factory/VendorFactoryTsvImpl.java
@@ -0,0 +1,11 @@
+package org.lawrencebower.docgen.web_logic.view.view_factory.tsv_factory;
+
+import org.lawrencebower.docgen.web_logic.view.contact.ContactView;
+import org.lawrencebower.docgen.web_logic.view.view_factory.factory.VendorFactory;
+
+public class VendorFactoryTsvImpl implements VendorFactory {
+    @Override
+    public ContactView getVendor() {
+        return null;
+    }
+}
